ab2d7ac49626c74f418977887262b41b5b5b16b9
boldCM <carolin@cc-meier.de>
2020-11-02 08:35:58 +0100

    Build function to update ToDos

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import CreateInputField from "./components/InputField";
 import React, { useState } from "react";
 
 function App() {
-  const [toDoArray, setToDoArray] = useState(
-    JSON.parse(localStorage.getItem("ToDos")) || []
-  );
+  const existingToDos = JSON.parse(localStorage.getItem("ToDos")) || [];
+  const [toDoArray, setToDoArray] = useState(existingToDos);
+  // Ich brauche noch eine Funktion, die mir beim
+  // das neue ToDo dem LocalStorage hinzufügt
+  function updateToDo(toDo) {
+    let existingToDos = null;
+    try {
+      existingToDos = JSON.parse(localStorage.getItem("ToDos"));
+    } catch (error) {
+      console.error(error);
+      existingToDos = [];
+    }
+
+    if (!existingToDos.includes(toDo)) {
+      const newToDo = [...existingToDos, toDo];
+      localStorage.setItem("ToDos", JSON.stringify(newToDo));
+      return newToDo;
+      // müsste ToDo hier nicht das inputValue aus dem UseState aus der InputField.js sein?
+    }
+    return existingToDos;
+  }
 
   return (
     <div className="App">
