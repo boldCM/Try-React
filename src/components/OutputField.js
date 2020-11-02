6b4ff2b9fa140d7e013fa0c1ed4ceb9a7c906dd7
boldCM <carolin@cc-meier.de>
2020-11-02 21:09:20 +0100

    Showing outputfield

--- src/components/OutputField.js
+++ src/components/OutputField.js
@@ -2,6 +2,11 @@ import "./OutputField.css";
 import React from "react";
 
 export default function CreateOutputField({ toDoArray }) {
+  //   const createList = (toDoArray) => {
+  //     toDoArray.map((item) => <li> {item} </li>);
+  //   };
+  // irgendwas war mit .map bzw ich will mehrere Zeilen
+
   return (
     <div className="outputField">
       {toDoArray}
